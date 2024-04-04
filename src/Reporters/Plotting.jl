adf47f05693a0ce98ee30c0fe700fcd70c6cc7b4
TheHedgehog2000 <jprentic@andrew.cmu.edu>
2024-04-04 08:44:42 -0400

    Added reporter plotting

--- src/Reporters/Plotting.jl
+++ src/Reporters/Plotting.jl
@@ -1,36 +1,30 @@
 using Plots
 using LaTeXStrings
-using TiffImages: load, save
 using NaturalSort: sort, natural
 using StatsBase
-using ColorTypes: Gray, N0f16
-using ImageMorphology: label_components, component_lengths, component_centroids
-using Images: imresize, distance_transform, feature_transform
-using HistogramThresholding: find_threshold, Otsu
-using FLoops
-using DelimitedFiles
 
 pgfplotsx()
 
 function main()
+    # Data are stored inside a folder and filename is "data.jld2" -- data are called "data"
 	push!(PGFPlotsX.CUSTOM_PREAMBLE, "\\usepackage{sfmath}\n\\renewcommand{\\familydefault}{\\sfdefault}")
     default(titlefont = 20, legendfontsize = 15, 
             guidefont = (20, :black), colorbar_tickfontsize=15, colorbar_titlefontsize=20, tickfont = (15, :black), 
             guide = L"x", linewidth=2, grid=false, formatter=:plain)
-    plot_size = (350,300)
     plot_xlabel = "Time (h)"
-    plot_ylabel = L"Distance from center ($\mu$m)"
-    plot_title = "Data"
+    plot_ylabel = "Reporter activity (a.u.)"
 
-    master_directory = "/mnt/h/Dispersal"
-	image_folders = filter(isdir, readdir(master_directory, join=true))
-    filter!(folder->folder≠master_directory*"/Plots", image_folders)
+    master_directory = "/mnt/f/Sandhya_Imaging/Time_Lapses/Reporters/"
+    data_folders = sort(filter(x -> occursin("data", x), readdir(master_directory, join=true)), lt=natural) 
     plots_folder = "/mnt/h/Dispersal/Plots"
-	plt = heatmap(data_matrix, xticks=xs, yticks=ys, color=c, clim=(-0.1, 0),
-				  colorbar_title="Density change (a.u.)", xformatter=xi -> xi*1/6, 
-				  yformatter=yi -> yi/ytick_interval*n, size=plot_size)
-	xlabel!(plt, plot_xlabel)
-	ylabel!(plt, plot_ylabel)
-	title!(plt, plot_title)
+    plot_size = (350,300*length(plot_folders))
+    p = plot(layout=(1,length(data_folders)), size=plot_size)
+    titles = [L"\textit{Ptac}", L"\textit{vpsL}", L"\textit{flaA}", L"\textit{luxC}"]
+
+    for i in length(data_folders)
+        data = load(data_folder*"data.jld2", "data")
+        x=1:length(data)
+        plot!(x, data, xlabel=plot_xlabel, ylabel=plot_ylabel, title=titles[i], xformatter=xi -> xi*1/2, subplot=i)
+    end
 	savefig("$plots_folder/$plot_filename"*".svg")
 end
